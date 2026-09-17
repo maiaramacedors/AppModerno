9c50a30f6e11ec4222d3e625439851dced92f37f
Maiara Macedo <maiaramacedo.r@outlook.com>
2026-09-17 13:25:47 -0300

    Primeiro commit - AppModerno

---
+++ app/src/main/java/com/example/appmoderno/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.appmoderno;
+
+import android.os.Bundle;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+    private MaterialButton btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        inicializarComponentes();
+        configurarEventos();
+    }
+    private void inicializarComponentes(){
+
+        btnLogin = findViewById(R.id.btnLogin);
+    }
+    private void configurarEventos(){
+        btnLogin.setOnClickListener(v ->
+        Toast.makeText(this, "Login realizado com sucesso", Toast.LENGTH_SHORT).show()
+        );
+    }
+}
